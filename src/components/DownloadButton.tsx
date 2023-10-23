c046ae983aa6912d3ea7c289eaa8fd1c665eefed
Aidan-R-1032 <agruiz6742@gmail.com>
2023-10-23 14:56:24 -0500

    Cleaned up a bit - still room for improvement

--- src/components/DownloadButton.tsx
+++ src/components/DownloadButton.tsx
@@ -1,19 +1,36 @@
-import { Button, ButtonGroup, Center, Stack } from '@chakra-ui/react'
+import { Button, Box, AbsoluteCenter, Container, Stack, Heading} from '@chakra-ui/react'
 
 function DownloadButton(){
-    let test = "/test.txt"
+    let test = "/test.txt" // ideally the zipped file
+    let files = [test]  // ideally - list of files to be zipped into the zipped file 
     const downloadFileAtURL = (url)=>{
 
-        const fileName = url.split("/").pop();
-        const aTag = document.createElement('a');
+        const fileName = url.split("/").pop();  // exclude the file path
+        const aTag = document.createElement('a');   // create a 'dummy' anchor element
         aTag.href = url;
-        aTag.setAttribute('download', fileName);
+        aTag.setAttribute('download', fileName);    // allow dummy element to be downloaded
         document.body.appendChild(aTag);
         aTag.click();
-        aTag.remove();
+        aTag.remove();  // remove dummy anchor
     }
     return (
-        <Button onClick={()=> downloadFileAtURL(test)}>Download Zip</Button>
+        <Container centerContent>
+            <Stack>
+                <Heading>
+                    Download Your Files Here!
+                </Heading>
+                <Box bg='green.100' padding={"10px"}>
+                    {files.map((f) => (
+                        <Box key={f} bg='green.400' textAlign={"center"} padding={"10px"}>{f.split("/").pop()}</Box> // display all the files to be zipped
+                    ))}
+                </Box>
+                <Box position='relative' h='100px'>
+                    <AbsoluteCenter bg='green.100' p='4' color='white' axis='both'>
+                        <Button onClick={()=> downloadFileAtURL(test)} justifyContent={"center"}>Download Zip</Button>
+                    </AbsoluteCenter>
+                </Box>
+            </Stack>
+        </Container>
     )
 }
 export default DownloadButton;
