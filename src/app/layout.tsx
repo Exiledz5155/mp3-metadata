762b289dcea249f416f4c17ac761ab725cfe7140
Nelson Trinh <147110240+ducttri@users.noreply.github.com>
2023-10-23 19:00:12 -0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,62 +1,109 @@
-// app/layout.tsx
+// app/providers.tsx
 "use client";
 
-import { Navbar } from "../components/Navbar";
-import { Providers } from "./providers";
-import { useSelectionContainer } from "@air/react-drag-to-select";
-import { Box, Flex, Spacer, Stack, HStack, VStack } from "@chakra-ui/react";
+import { CacheProvider } from "@chakra-ui/next-js";
+import { Providers } from "../providers";
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  Box,
+  Center,
+  Grid,
+  GridItem,
+  Flex,
+  Image,
+  Button,
+  VStack,
+  Text,
+  Divider,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  NumberIncrementStepper,
+  NumberDecrementStepper,
+  useColorModeValue,
+  useColorMode,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  Badge,
+  ColorModeScript
+} from "@chakra-ui/react";
 
-interface Props {
-  children: React.ReactNode;
+// THIS IS TEMPLATE CODE FOR STARTING A NEW PAGE
+// DO NOT MODIFY OR DELETE - Danny
+
+function MusicCard() {
+  const music = {
+    imageUrl: 'https://nektony.com/wp-content/uploads/2019/08/spotify-icon.png',
+    imageAlt: 'Music file Icon',
+    title: 'Song Title',
+    artist: 'Various Artists'
+  }
+
+  return (
+    <Box maxW='sm' borderWidth='1px' borderRadius='lg' h='60px' overflow='hidden'>
+      <Grid 
+        h='100%'
+        w='100%'
+        templateRows='repeat(2, 1fr)'
+        templateColumns='repeat(5, 1fr)'
+        gap={0}
+      >
+        <GridItem rowSpan={2} colSpan={1} bg={useColorModeValue("green.400", "gray.900")}>
+          <Center w='60px' h='60px'>
+            <Image src={music.imageUrl} alt={music.imageAlt} boxSize='50px'/>
+          </Center>
+        </GridItem>
+        <GridItem colSpan={4}>{music.title}</GridItem>
+        <GridItem colSpan={4}>{music.artist}</GridItem>
+      </Grid>
+    </Box>
+  )
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  const fileHub = ["Song1", "Song2", "Song2", "Song2"];
 
-  const fileHubLink = (props: Props) => {
-    const { children } = props;
-    console.log(typeof children);
-    return;
-  };
-
-  const { DragSelection } = useSelectionContainer();
+export default function Download({ children }: { children: React.ReactNode }) {
+  const fileHub = ["Song1", "Song2", "Song2", "Song2", "Song2", "Song2", "Song2", "Song2", "Song2", "Song2", "Song2", "Song2", "Song2", "Song2", "Song2", "Song2", "Song2", "Song2", "Song2", "Song2", "Song2", "Song2", "Song2"];
   return (
-    <html lang="en">
-      <body>
-        {/* For styling to work, components need to be placed under providers */}
-        <Providers>
-          <Navbar />
+    <CacheProvider>
+      {/* Place stuff above providers */}
 
-          {/* Refactor this to it's own component */}
-          <DragSelection />
-          <Flex h="125vh">
-            <Box p="4" bg="#daffe4" h="100%" w="15%">
-              FileHub
-              {/* TODO: How to represent files */}
-              <VStack spacing="2px">
-                {fileHub.map((file) => (
-                  <Box
-                    p="4"
-                    bg="green.300"
-                    border="2px"
-                    borderColor="gray.600"
-                    key={file}
-                    w="100%"
-                  >
-                    {file}
-                  </Box>
-                ))}
-              </VStack>
-            </Box>
+      <Grid 
+        h='100%'
+        w='100%'
+        templateRows='repeat(2, 1fr)'
+        templateColumns='repeat(5, 1fr)'
+        gap={4}
+      >
+        <GridItem rowSpan={2} colSpan={1} bg={useColorModeValue("green.400", "gray.900")}>
+        <Box maxHeight="100%" overflowY="auto">
+          <Table colorScheme="green" overflowY="auto">
+            <Thead  top={0}>
+              <Tr>
+                <Th>File List</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {fileHub.map((file) => (
+                <Tr>
+                  <MusicCard></MusicCard>
+                </Tr>
+              ))}
+            </Tbody>
+          </Table>
+        </Box>
 
-            {children}
-          </Flex>
-        </Providers>
-      </body>
-    </html>
+        </GridItem>
+        <GridItem colSpan={2} bg='papayawhip' />
+        <GridItem colSpan={2} bg='papayawhip' />
+        <GridItem colSpan={4} bg='tomato' />
+      </Grid>
+      <Providers>{children}</Providers>
+    </CacheProvider>
   );
 }
