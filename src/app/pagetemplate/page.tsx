24f072c711b58033426a7ff62dbafd55e39924ea
Nelson Trinh <147110240+ducttri@users.noreply.github.com>
2023-10-23 19:01:17 -0500

    Update page.tsx

--- src/app/pagetemplate/page.tsx
+++ src/app/pagetemplate/page.tsx
@@ -2,7 +2,7 @@
 "use client";
 
 import { CacheProvider } from "@chakra-ui/next-js";
-
+import { Providers } from "../providers";
 import {
   FormControl,
   FormLabel,
@@ -22,104 +22,87 @@ import {
   NumberInputStepper,
   NumberIncrementStepper,
   NumberDecrementStepper,
+  useColorModeValue,
+  useColorMode,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  Badge,
+  ColorModeScript
 } from "@chakra-ui/react";
-import { NumberBox } from "../../components/NumberBox";
-import { Providers } from "../providers";
 
 // THIS IS TEMPLATE CODE FOR STARTING A NEW PAGE
 // DO NOT MODIFY OR DELETE - Danny
 
+function MusicCard() {
+  const music = {
+    imageUrl: 'https://nektony.com/wp-content/uploads/2019/08/spotify-icon.png',
+    imageAlt: 'Music file Icon',
+    title: 'Song Title',
+    artist: 'Various Artists'
+  }
+
+  return (
+    <Box maxW='sm' borderWidth='1px' borderRadius='lg' h='60px' overflow='hidden'>
+      <Grid 
+        h='100%'
+        w='100%'
+        templateRows='repeat(2, 1fr)'
+        templateColumns='repeat(5, 1fr)'
+        gap={0}
+      >
+        <GridItem rowSpan={2} colSpan={1} bg={useColorModeValue("green.400", "gray.900")}>
+          <Center w='60px' h='60px'>
+            <Image src={music.imageUrl} alt={music.imageAlt} boxSize='50px'/>
+          </Center>
+        </GridItem>
+        <GridItem colSpan={4}>{music.title}</GridItem>
+        <GridItem colSpan={4}>{music.artist}</GridItem>
+      </Grid>
+    </Box>
+  )
+}
+
+
 export default function Download({ children }: { children: React.ReactNode }) {
+  const fileHub = ["Song1", "Song2", "Song2", "Song2", "Song2", "Song2", "Song2", "Song2", "Song2", "Song2", "Song2", "Song2", "Song2", "Song2", "Song2", "Song2", "Song2", "Song2", "Song2", "Song2", "Song2", "Song2", "Song2"];
   return (
     <CacheProvider>
       {/* Place stuff above providers */}
-      <Grid templateColumns="repeat(3, 1fr)" gap={6} w="100%">
-        <GridItem w="100%">
-          <Center>
-            <Flex
-              align="center"
-              justify="center"
-              mt="50px"
-              maxW="sm"
-              bg="green.400"
-              w="100%"
-              p={2}
-              color="white"
-              borderRadius={10}
-            >
-              <VStack spacing={40} align="stretch">
-                <Center>
-                  <Flex p="10px" maxW="300px">
-                    <Image src="https://m.media-amazon.com/images/I/71NUQhdZDJL._UF1000,1000_QL80_.jpg" />
-                  </Flex>
-                </Center>
-                <Button colorScheme="gray">Upload</Button>
-              </VStack>
-            </Flex>
-          </Center>
+
+      <Grid 
+        h='100%'
+        w='100%'
+        templateRows='repeat(2, 1fr)'
+        templateColumns='repeat(5, 1fr)'
+        gap={4}
+      >
+        <GridItem rowSpan={2} colSpan={1} bg={useColorModeValue("green.400", "gray.900")}>
+        <Box maxHeight="100%" overflowY="auto">
+          <Table colorScheme="green" overflowY="auto">
+            <Thead  top={0}>
+              <Tr>
+                <Th>File List</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {fileHub.map((file) => (
+                <Tr>
+                  <MusicCard></MusicCard>
+                </Tr>
+              ))}
+            </Tbody>
+          </Table>
+        </Box>
+
         </GridItem>
-
-        <GridItem w="100%">
-          <Center>
-            <Flex
-              align="center"
-              justify="center"
-              mt="50px"
-              maxW="sm"
-              bg="green.400"
-              w="100%"
-              p={2}
-              color="white"
-              borderRadius={10}
-            >
-              <FormControl>
-                <FormLabel>Song Title</FormLabel>
-                <Input type="text" />
-
-                <FormLabel>Album Title</FormLabel>
-                <Input type="text" />
-
-                <FormLabel>Album Artist</FormLabel>
-                <Input type="text" />
-
-                <FormLabel>Artist(s)</FormLabel>
-                <Input type="text" />
-
-                <FormLabel>Year</FormLabel>
-                <NumberBox defaultValue={2000} min={0} max={3000} />
-
-                <FormLabel>Genre</FormLabel>
-                <Input type="text" />
-
-                <FormLabel>Track #</FormLabel>
-                <NumberBox defaultValue={0} min={0} max={3000} />
-              </FormControl>
-            </Flex>
-          </Center>
-        </GridItem>
-
-        <GridItem w="100%">
-          <Center>
-            <Flex
-              mt="50px"
-              maxW="sm"
-              bg="green.400"
-              w="100%"
-              p={2}
-              color="white"
-              borderRadius={10}
-            >
-              <VStack spacing={5} align="stretch">
-                <Text>Bitrate: 128 Kb/s</Text>
-                <Text>Length: 4 minutes and 31 seconds</Text>
-                <Text>Date Created: March 15, 2015</Text>
-                <Text>Date Modified: Today</Text>
-              </VStack>
-            </Flex>
-          </Center>
-        </GridItem>
+        <GridItem colSpan={2} bg='papayawhip' />
+        <GridItem colSpan={2} bg='papayawhip' />
+        <GridItem colSpan={4} bg='tomato' />
       </Grid>
-
       <Providers>{children}</Providers>
     </CacheProvider>
   );
