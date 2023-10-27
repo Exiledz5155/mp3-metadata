52f8b532da36f8ba3be8596804c6cc0c800c353b
Aidan-R-1032 <agruiz6742@gmail.com>
2023-10-26 22:27:25 -0500

    download selection logic - hooks are fun

--- src/components/DownloadButton.tsx
+++ src/components/DownloadButton.tsx
@@ -7,33 +7,37 @@ import {
   Heading,
 } from "@chakra-ui/react";
 
-function DownloadButton() {
-  let test = "/test.txt"; // ideally the zipped file
-  let files = [test, "another_test.txt"]; // ideally - list of files to be zipped into the zipped file
-  const downloadFileAtURL = (url) => {
-    const fileName = url.split("/").pop(); // exclude the file path
-    const aTag = document.createElement("a"); // create a 'dummy' anchor element
-    aTag.href = url;
-    aTag.setAttribute("download", fileName); // allow dummy element to be downloaded
-    document.body.appendChild(aTag);
-    aTag.click();
-    aTag.remove(); // remove dummy anchor
-  };
+function download(files){
+  let test = files[0]; // ideally the zipped file
+  const fileName = test.split("/").pop(); // exclude the file path
+  const aTag = document.createElement("a"); // create a 'dummy' anchor element
+  aTag.href = test;
+  aTag.setAttribute("download", fileName); // allow dummy element to be downloaded
+  document.body.appendChild(aTag);
+  aTag.click();
+  aTag.remove(); // remove dummy anchor
+};
+
+let DownloadButton = ({selected_files})=> {
+  console.log('inside download button');
+  console.log(selected_files);
+  console.log(typeof(selected_files))
   return (
     <Container centerContent>
       <Stack>
         <Heading>Download Your Files Here!</Heading>
         <Box bg="green.100" padding={"10px"}>
-          {files.map((f) => (
+          {selected_files.map((f) => {
+            return (
             <Box key={f} bg="green.400" textAlign={"center"} padding={"10px"}>
               {f.split("/").pop()}
             </Box> // display all the files to be zipped
-          ))}
+          )})}
         </Box>
         <Box position="relative" h="100px">
           <AbsoluteCenter bg="green.100" p="4" color="white" axis="both">
             <Button
-              onClick={() => downloadFileAtURL(test)}
+              onClick={() => download(selected_files)}
               justifyContent={"center"}
             >
               Download Zip
--- src/components/DownloadHub.tsx
+++ src/components/DownloadHub.tsx
@@ -19,6 +19,13 @@ const StyledText = styled.h1`
   font-size: 30px;
 `;
 
+type DownloadHubProps = {
+  selected_files: string[];
+  fileProps: string[][];
+  adder: (file: string) => void;
+  remover: (file: string) => void;
+};
+
 // CHAKRA UI STYLED TEXT, DOESN'T WORK, WE WANT TO SWITCH TO THIS
 const theme = extendTheme({
   textStyles: {
@@ -38,167 +45,11 @@ const theme = extendTheme({
   },
 });
 
-export default function DownloadHub() {
+export default function DownloadHub({selected_files, fileProps, adder, remover}: DownloadHubProps) {
   // Creates mock song data
-  const fileProps = [
-    [
-      "https://www.graphicdesignforum.com/uploads/default/original/2X/3/351b46d8082a31a5cbbf062a8425dcdbeddcabba.jpeg",
-      "image_alt",
-      "Song 1",
-      "Artist 1",
-    ],
-    [
-      "https://images.theconversation.com/files/512871/original/file-20230301-26-ryosag.jpg?ixlib=rb-1.1.0&rect=97%2C79%2C5799%2C5817&q=45&auto=format&w=926&fit=clip",
-      "image_alt",
-      "Song 2",
-      "Artist 2",
-    ],
-    [
-      "https://www.graphicdesignforum.com/uploads/default/original/2X/d/d3c4e744046205a49d06beb874df3b39da7c9c73.jpeg",
-      "image_alt",
-      "Song 3",
-      "Artist 3",
-    ],
-    [
-      "https://www.graphicdesignforum.com/uploads/default/original/2X/8/8f2f98d3793cc6aa38d7566340bd346156523ccd.jpeg",
-      "image_alt",
-      "Song 4",
-      "Artist 4",
-    ],
-    [
-      "https://www.graphicdesignforum.com/uploads/default/original/2X/8/8f2f98d3793cc6aa38d7566340bd346156523ccd.jpeg",
-      "image_alt",
-      "Song 5",
-      "Artist 4",
-    ],
-    [
-      "https://www.graphicdesignforum.com/uploads/default/original/2X/8/8f2f98d3793cc6aa38d7566340bd346156523ccd.jpeg",
-      "image_alt",
-      "Song 6",
-      "Artist 4",
-    ],
-    [
-      "https://www.graphicdesignforum.com/uploads/default/original/2X/8/8f2f98d3793cc6aa38d7566340bd346156523ccd.jpeg",
-      "image_alt",
-      "Song 7",
-      "Artist 4",
-    ],
-    [
-      "https://www.graphicdesignforum.com/uploads/default/original/2X/8/8f2f98d3793cc6aa38d7566340bd346156523ccd.jpeg",
-      "image_alt",
-      "Song 8",
-      "Artist 4",
-    ],
-    [
-      "https://www.graphicdesignforum.com/uploads/default/original/2X/8/8f2f98d3793cc6aa38d7566340bd346156523ccd.jpeg",
-      "image_alt",
-      "Song 9",
-      "Artist 4",
-    ],
-    [
-      "https://www.graphicdesignforum.com/uploads/default/original/2X/8/8f2f98d3793cc6aa38d7566340bd346156523ccd.jpeg",
-      "image_alt",
-      "Song 10",
-      "Artist 4",
-    ],
-    [
-      "https://www.graphicdesignforum.com/uploads/default/original/2X/8/8f2f98d3793cc6aa38d7566340bd346156523ccd.jpeg",
-      "image_alt",
-      "Song 11",
-      "Artist 4",
-    ],
-    [
-      "https://www.graphicdesignforum.com/uploads/default/original/2X/8/8f2f98d3793cc6aa38d7566340bd346156523ccd.jpeg",
-      "image_alt",
-      "Song 12",
-      "Artist 4",
-    ],
-    [
-      "https://www.graphicdesignforum.com/uploads/default/original/2X/8/8f2f98d3793cc6aa38d7566340bd346156523ccd.jpeg",
-      "image_alt",
-      "Song 13",
-      "Artist 4",
-    ],
-    [
-      "https://www.graphicdesignforum.com/uploads/default/original/2X/8/8f2f98d3793cc6aa38d7566340bd346156523ccd.jpeg",
-      "image_alt",
-      "Song 14",
-      "Artist 4",
-    ],
-    [
-      "https://www.graphicdesignforum.com/uploads/default/original/2X/8/8f2f98d3793cc6aa38d7566340bd346156523ccd.jpeg",
-      "image_alt",
-      "Song 15",
-      "Artist 4",
-    ],
-    [
-      "https://www.graphicdesignforum.com/uploads/default/original/2X/8/8f2f98d3793cc6aa38d7566340bd346156523ccd.jpeg",
-      "image_alt",
-      "Song 16",
-      "Artist 4",
-    ],
-    [
-      "https://www.graphicdesignforum.com/uploads/default/original/2X/8/8f2f98d3793cc6aa38d7566340bd346156523ccd.jpeg",
-      "image_alt",
-      "Song 17",
-      "Artist 4",
-    ],
-    [
-      "https://www.graphicdesignforum.com/uploads/default/original/2X/8/8f2f98d3793cc6aa38d7566340bd346156523ccd.jpeg",
-      "image_alt",
-      "Song 18",
-      "Artist 4",
-    ],
-    [
-      "https://www.graphicdesignforum.com/uploads/default/original/2X/8/8f2f98d3793cc6aa38d7566340bd346156523ccd.jpeg",
-      "image_alt",
-      "Song 19",
-      "Artist 4",
-    ],
-    [
-      "https://www.graphicdesignforum.com/uploads/default/original/2X/8/8f2f98d3793cc6aa38d7566340bd346156523ccd.jpeg",
-      "image_alt",
-      "Song 20",
-      "Artist 4",
-    ],
-    [
-      "https://www.graphicdesignforum.com/uploads/default/original/2X/8/8f2f98d3793cc6aa38d7566340bd346156523ccd.jpeg",
-      "image_alt",
-      "Song 21",
-      "Artist 4",
-    ],
-    [
-      "https://www.graphicdesignforum.com/uploads/default/original/2X/8/8f2f98d3793cc6aa38d7566340bd346156523ccd.jpeg",
-      "image_alt",
-      "Song 22",
-      "Artist 4",
-    ],
-    [
-      "https://www.graphicdesignforum.com/uploads/default/original/2X/8/8f2f98d3793cc6aa38d7566340bd346156523ccd.jpeg",
-      "image_alt",
-      "Song 23",
-      "Artist 4",
-    ],
-    [
-      "https://www.graphicdesignforum.com/uploads/default/original/2X/8/8f2f98d3793cc6aa38d7566340bd346156523ccd.jpeg",
-      "image_alt",
-      "Song 24",
-      "Artist 4",
-    ],
-    [
-      "https://www.graphicdesignforum.com/uploads/default/original/2X/8/8f2f98d3793cc6aa38d7566340bd346156523ccd.jpeg",
-      "image_alt",
-      "Song 25",
-      "Artist 4",
-    ],
-    [
-      "https://www.graphicdesignforum.com/uploads/default/original/2X/8/8f2f98d3793cc6aa38d7566340bd346156523ccd.jpeg",
-      "image_alt",
-      "Song 26",
-      "Artist 4",
-    ],
-  ];
-
+  console.log("Inside DownloadHub")
+  console.log(fileProps);
+  console.log(selected_files);
   return (
     <Box bg={useColorModeValue("green.400", "gray.900")}>
       <Box maxHeight="100%" overflowY="auto">
@@ -213,13 +64,25 @@ export default function DownloadHub() {
           {/* colormode doesn't seem to respond correctly for Tbody */}
           <Tbody bg={useColorModeValue("green.400", "gray.900")}>
             {/* Maps the mock data and creates individual MusicCards */}
-            {fileProps.map((file) => (
+            {fileProps.map((file) => {
+              return (
               // Make sure backend checks for metadata matching
               // Since if 2 songs share the same title, they share the same key
-              <Tr key={file[2]}>
-                <MusicCard props={file}></MusicCard>
-              </Tr>
-            ))}
+                <Tr key={file[2]} onClick={()=>{
+                  console.log("You clicked on me!")
+                  let file_index = selected_files.indexOf(file[2]);
+                  if(file_index == -1){ // file isn't selected, so add it
+                    adder(file[2]);
+                    console.log("Added the file " + file[2])
+                  }
+                  else{
+                    remover(file[2]);
+                    console.log("Removed the file " + file[2])
+                  }
+                }}>
+                    <MusicCard props={file}></MusicCard>  
+                </Tr>
+            )})}
           </Tbody>
         </Table>
       </Box>
