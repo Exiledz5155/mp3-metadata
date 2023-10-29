7189a97e6f7f8a6866d67db8851e0ba28704154c
Danny Bui <dannybui5155@gmail.com>
2023-10-29 17:21:13 -0500

    fixed import errors

--- src/app/uploadtest/page.tsx
+++ src/app/uploadtest/page.tsx
@@ -4,11 +4,7 @@
 import { CacheProvider } from "@chakra-ui/next-js";
 import { Providers } from "../providers";
 import { CardView2 } from "../../components/EditPage";
-import { CardView3 } from "../../components/Card3";
-import DownloadHub from "../../components/DownloadHub";
 import { Box, useColorModeValue } from "@chakra-ui/react";
-
-
 
 // THIS IS TEMPLATE CODE FOR STARTING A NEW PAGE
 // DO NOT MODIFY OR DELETE - Danny
@@ -17,15 +13,13 @@ export default function Download({ children }: { children: React.ReactNode }) {
   return (
     <CacheProvider>
       <Box bg={useColorModeValue("green.400", "gray.900")}>
-      <Box maxHeight="100%" overflowY="auto">
+        <Box maxHeight="100%" overflowY="auto">
           what up gang
-      </Box>
+        </Box>
       </Box>
 
+      <CardView2></CardView2>
 
-      
-        <CardView2></CardView2>
-      
       <Providers>{children}</Providers>
     </CacheProvider>
   );
