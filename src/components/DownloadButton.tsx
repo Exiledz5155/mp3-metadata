0d5b5ae44661dad0ead5983170d38ae458271d8a
Aidan-R-1032 <agruiz6742@gmail.com>
2023-11-01 22:44:19 -0500

    true working download button

--- src/components/DownloadButton.tsx
+++ src/components/DownloadButton.tsx
@@ -15,20 +15,12 @@ function download(files) {
     const fileName = (f["filePath"]).split("/").pop();
     console.log(fileName)
     const aTag = document.createElement("a");
-    const filePath = f["filePath"]
-    console.log(filePath);
-    // create a blob object from the file data
-    const blob = new Blob([f["raw"]], {type: "audio/mpeg"});
-    // create a blob URL from the blob object
-    const blobURL = URL.createObjectURL(blob);
-    // use the blob URL as the href attribute
-    aTag.href = blobURL;
+    // use the file path as the href attribute
+    aTag.href = "tmp/" + fileName;
     aTag.setAttribute("download", fileName);
     document.body.appendChild(aTag);
     aTag.click();
     aTag.remove(); // remove dummy anchor
-    // revoke the blob URL to free memory
-    URL.revokeObjectURL(blobURL);
   }
 }
 
@@ -43,7 +35,7 @@ let DownloadButton = ({ selected_files }: DownloadButtonProps) => {
     <Container centerContent>
       <Stack>
         <Heading>Download Your Files Here!</Heading>
-        <Box bg="green.100" padding={"10px"}>
+        <Box bg="green.100" padding={"10px"} margin="30px">
           {selected_files.map((f) => {
             return (
               <Box key={f["title"]} bg="green.400" textAlign={"center"} padding={"10px"} margin={"10px"}>
@@ -52,8 +44,8 @@ let DownloadButton = ({ selected_files }: DownloadButtonProps) => {
             );
           })}
         </Box>
-        <Box position="relative" h="100px">
-          <AbsoluteCenter bg="green.100" p="4" color="white" axis="both">
+        <Box position="relative" bg="green.100" margin="30px">
+          <AbsoluteCenter bg="green.400" p="4" color="white" axis="both">
             <Button
               onClick={() => download(selected_files)}
               justifyContent={"center"}
