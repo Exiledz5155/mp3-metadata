370f03bc90f92f818e5746bb14e3677b8215909c
Danny Bui <Dannybui5155@gmail.com>
2024-01-28 01:20:20 -0600

    removed redundant imports album-view

--- src/app/editor/album-view/page.tsx
+++ src/app/editor/album-view/page.tsx
@@ -1,20 +1,7 @@
 // app/providers.tsx
 "use client";
 
-import { CacheProvider } from "@chakra-ui/next-js";
 import { Providers } from "../../providers";
-import { EditCardView } from "../../../components/EditCardView";
-import {
-  Box,
-  Container,
-  Flex,
-  HStack,
-  useColorModeValue,
-} from "@chakra-ui/react";
-import { EditForm } from "../../../components/EditForm";
-import { useState, useEffect } from "react";
-import DownloadHub from "../../../components/DownloadHub";
-import { AlbumDisplay } from "../../../components/AlbumDisplay";
 import { SongDisplay } from "../../../components/SongDisplay";
 
 // THIS IS TEMPLATE CODE FOR STARTING A NEW PAGE
