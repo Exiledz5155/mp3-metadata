59c7f42c6b211bc4b87b1bc4044131944d1440e1
Calvin G Duddingston <duddi021@umn.edu>
2024-02-16 12:22:55 -0600

    Session Storage works to add in-session uploads and relay that information from local client.

--- src/components/viewUploads.tsx
+++ src/components/viewUploads.tsx
@@ -1,11 +1,29 @@
 // viewUploads.tsx
-import React from 'react';
+"use client"
+import { useEffect, useState } from 'react';
+
+interface UploadedFile {
+  fileName: string;
+  timestamp: string; // Assuming timestamp is a string, you can adjust the type accordingly
+}
 
 export default function UploadedFilesList() {
-  const uploadedFiles = JSON.parse(sessionStorage.getItem('uploadedFiles') || '[]');
+  const [uploadedFiles, setUploadedFiles] = useState<UploadedFile[]>([]);
+
+  useEffect(() => {
+    // Check if window is defined to make sure this runs on the client side
+    if (typeof window !== 'undefined') {
+      console.log("sessionStorage is avaiable");
+      const storedFiles = JSON.parse(sessionStorage.getItem('uploadedFiles') || '[]');
+      setUploadedFiles(storedFiles);
+    }
+    else {
+      console.log("session storage is unavailable");
+    }
+  }, []);
 
   return (
-    <div>
+    <div style={{ color: 'white' }}>
       <h2>Uploaded Files</h2>
       <ul>
         {uploadedFiles.map((file, index) => (
@@ -15,4 +33,3 @@ export default function UploadedFilesList() {
     </div>
   );
 };
-
