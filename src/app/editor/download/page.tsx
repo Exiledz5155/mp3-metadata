a09f4187823d750db5e82272735c0bb9fbb0a330
Danny Bui <Dannybui5155@gmail.com>
2024-02-17 20:39:09 -0600

    fixed default export issue

--- src/app/editor/download/page.tsx
+++ src/app/editor/download/page.tsx
@@ -1,25 +1,32 @@
-if (typeof window !== 'undefined') {
-  document.getElementById('downloadButton').addEventListener('click', async () => {
-      try {
-        // Fetch the download URL from your backend server
-        const response = await fetch('/getBlobDownloadUrl');
-        if (!response.ok) {
-          throw new Error('Failed to fetch download URL');
-        }
-        const { downloadUrl } = await response.json();
-    
-        // Create a temporary anchor element
-        const anchor = document.createElement('a');
-        anchor.href = downloadUrl;
-    
-        // Set the download attribute to force download and set the filename
-        anchor.download = 'blobFilename.ext'; // Set the filename as per your requirement
-    
-        // Programmatically click the anchor to trigger the download
-        anchor.click();
-      } catch (error) {
-        console.error('Error:', error);
-        // Handle errors appropriately, e.g., display an error message to the user
+"use client";
+
+export default function download() {
+  return (
+    if (typeof window !== 'undefined') {
+      document.getElementById('downloadButton').addEventListener('click', async () => {
+          try {
+            // Fetch the download URL from your backend server
+            const response = await fetch('/getBlobDownloadUrl');
+            if (!response.ok) {
+              throw new Error('Failed to fetch download URL');
+            }
+            const { downloadUrl } = await response.json();
+        
+            // Create a temporary anchor element
+            const anchor = document.createElement('a');
+            anchor.href = downloadUrl;
+        
+            // Set the download attribute to force download and set the filename
+            anchor.download = 'blobFilename.ext'; // Set the filename as per your requirement
+        
+            // Programmatically click the anchor to trigger the download
+            anchor.click();
+          } catch (error) {
+            console.error('Error:', error);
+            // Handle errors appropriately, e.g., display an error message to the user
+          }
+        });
       }
-    });
-  }
+  );
+}
+
