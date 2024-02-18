df743ae3aab1162b854df603415e6a626a0eb2cb
Danny Bui <Dannybui5155@gmail.com>
2024-02-17 20:13:59 -0600

    brought back original fileupload, renamed current to UUIDFileUpload, added link buttons to and from upload and view-upload

--- src/app/editor/view-uploads/page.tsx
+++ src/app/editor/view-uploads/page.tsx
@@ -1,9 +1,20 @@
+"use client";
+
+import { Button } from "@chakra-ui/react";
+import Link from "next/link";
 import UploadedFilesList from "../../../components/viewUploads"; // Adjust the path based on your file structure
-export default function Home() {
+
+export default function viewUploads() {
+  // Bug for frontend to fix:
+  // Might not be relevant to fix at later, but the + icon for upload files
+  // In filehub is shifted to the left
   return (
     <div>
       <h1>Welcome to the Uploaded Files List Page </h1>
       <UploadedFilesList />
+      <Link href="/editor/upload">
+        <Button>Go to upload</Button>
+      </Link>
     </div>
   );
 }
