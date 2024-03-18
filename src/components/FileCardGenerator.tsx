a6d4bf8ec2aaf27d4bcb16a5a6bafdbf0280c4a8
Nick Hinds <nicholashinds2@gmail.com>
2024-03-18 16:48:57 -0500

    Created file upload card component generator

--- src/components/FileCardGenerator.tsx
+++ src/components/FileCardGenerator.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Flex,
   Button,
@@ -11,23 +11,59 @@ import {
   Icon,
   Box,
   Text,
+  FormControl,
+  FormLabel,
+  Input,
+  Switch,
 } from "@chakra-ui/react";
-import FileUploadCard from "./FileUploadCard";
 import { MdOutlineFilePresent } from "react-icons/md";
-import { IoCloudUploadOutline } from "react-icons/io5";
+import FileUploadCard from "./FileUploadCard";
 
-interface UploadBoxProps {
-  isOpen: boolean; // Whether the modal is open or not
-  onClose: () => void; // Function to close the modal
+interface FileCardGeneratorProps {
+  isCardOpen: boolean; // Whether the FileCardGenerator modal is open or not
+  onCardClose: () => void; // Function to close the FileCardGenerator modal
 }
-export const FileUploadBox: React.FC<UploadBoxProps> = ({
-  isOpen,
-  onClose,
+
+export const FileCardGenerator: React.FC<FileCardGeneratorProps> = ({
+  isCardOpen,
+  onCardClose,
 }) => {
+  const [fileName, setFileName] = useState("");
+  const [fileType, setFileType] = useState("");
+  const [fileSizeInBytes, setFileSizeInBytes] = useState(0);
+  const [uploadFailed, setUploadFailed] = useState(false);
+  const [inProgress, setInProgress] = useState(false);
+  const [progressValue, setProgressValue] = useState(0);
+
+  const handleGenerate = () => {
+    // Create a new FileUploadCard component with the provided values
+    const newFileUploadCard = (
+      <FileUploadCard
+        fileName={fileName}
+        fileType={fileType}
+        fileSizeInBytes={fileSizeInBytes}
+        uploadFailed={uploadFailed}
+        inProgress={inProgress}
+        progressValue={progressValue}
+      />
+    );
+
+    // Render the newly generated FileUploadCard component or take other actions as needed
+    console.log("Generated FileUploadCard:", newFileUploadCard);
+
+    // Close the modal
+    onCardClose();
+  };
+
   return (
     <>
       {/* Modal component */}
-      <Modal isOpen={isOpen} onClose={onClose} size="lg" closeOnOverlayClick={false}>
+      <Modal
+        isOpen={isCardOpen}
+        onClose={onCardClose}
+        size="lg"
+        closeOnOverlayClick={false}
+      >
         <ModalOverlay />
         <ModalContent
           bg={"brand.200"}
@@ -40,132 +76,80 @@ export const FileUploadBox: React.FC<UploadBoxProps> = ({
             <Flex alignItems="center">
               <Icon as={MdOutlineFilePresent} boxSize={8} />
               <Box fontSize="2xl" ml={2}>
-                Upload Files
+                Generate Files
               </Box>
             </Flex>
-            <ModalCloseButton
-              position="absolute"
-              top="28px" 
-              right="25px"
-              size="md"
-            />
+            <ModalCloseButton position="absolute" top="28px" right="25px" size="md" />
           </ModalHeader>
           <ModalBody pb={0}>
-            {/* File upload box */}
-            <Box border="2px dashed" p={4} borderRadius="2xl">
-              <Flex
-                direction="column"
-                justifyContent="center"
-                alignItems="center"
-              >
-                <Icon as={IoCloudUploadOutline} boxSize={12} mb={2} />
-                <Text fontWeight="bold">Drag and drop files here</Text>
-                <Text color="#8E95A3">or</Text>
-                {/* <Button
-                  as="label"
-                  htmlFor="fileInput"
-                  variant="solid"
-                  mt={2}
-                  bgGradient="linear(to-r, linear.100, linear.200)"
-                  _hover={{
-                    cursor: "pointer",
-                    color: "white",
-                    bg: "brand.300",
-                  }}
-                  rounded={"xl"}
-                  color="brand.200"
-                >
-                  Browse Files
-                  <Input
-                    type="file"
-                    id="fileInput"
-                    style={{ display: "none" }}
+            {/* File upload form */}
+            <Box border="2px dashed" p={4} borderRadius="2xl" mb={4}>
+              <FormControl>
+                <FormLabel>File Name</FormLabel>
+                <Input
+                  type="text"
+                  placeholder="Enter file name"
+                  value={fileName}
+                  onChange={(e) => setFileName(e.target.value)}
+                />
+              </FormControl>
+              <FormControl mt={4}>
+                <FormLabel>File Type</FormLabel>
+                <Input
+                  type="text"
+                  placeholder="Enter file type"
+                  value={fileType}
+                  onChange={(e) => setFileType(e.target.value)}
+                />
+              </FormControl>
+              <FormControl mt={4}>
+                <FormLabel>File Size (in bytes)</FormLabel>
+                <Input
+                  type="number"
+                  placeholder="Enter file size"
+                  value={fileSizeInBytes}
+                  onChange={(e) => setFileSizeInBytes(parseInt(e.target.value))}
+                />
+              </FormControl>
+              <Flex mt={4}>
+                <FormControl display="flex" alignItems="center" mr={4}>
+                  <FormLabel htmlFor="uploadFailed" mb="0">
+                    Upload Failed
+                  </FormLabel>
+                  <Switch
+                    id="uploadFailed"
+                    isChecked={uploadFailed}
+                    onChange={() => setUploadFailed(!uploadFailed)}
                   />
-                </Button> */}
-                <Button
-                  as="label"
-                  htmlFor="fileInput"
-                  variant="solid"
-                  mt={2}
-                  bgGradient="linear(to-r, linear.100, linear.200)"
-                  _hover={{
-                    cursor: "pointer",
-                    color: "white",
-                    bg: "brand.300",
-                  }}
-                  rounded={"xl"}
-                  color="brand.200"
-                >
-                  Browse Files
-                </Button>
-                {/* TODO: Link file uploads for button/drag + drop */}
+                </FormControl>
+                <FormControl display="flex" alignItems="center" mr={4}>
+                  <FormLabel htmlFor="inProgress" mb="0">
+                    In Progress
+                  </FormLabel>
+                  <Switch
+                    id="inProgress"
+                    isChecked={inProgress}
+                    onChange={() => {
+                      setInProgress(!inProgress);
+                      setProgressValue(0); // Reset progress value when toggled off
+                    }}
+                  />
+                </FormControl>
               </Flex>
+              {inProgress && (
+                <FormControl mt={4}>
+                  <FormLabel>Progress Value (0-100)</FormLabel>
+                  <Input
+                    type="number"
+                    placeholder="Enter progress value"
+                    min={0}
+                    max={100}
+                    value={progressValue}
+                    onChange={(e) => setProgressValue(parseInt(e.target.value))}
+                  />
+                </FormControl>
+              )}
             </Box>
-            <Text color="#8E95A3" mt={3}>
-              Only .mp3 files. Max size 30mb.
-            </Text>
-
-            {/* File list */}
-            <ModalHeader pl={0}>
-              <Box fontSize="l">Uploaded Files</Box>
-            </ModalHeader>
-            <Box
-              mb={5}
-              overflowY="auto"
-              maxHeight="275px"
-              paddingRight="10px"
-              // Custom scrollbar
-              css={{
-                "&::-webkit-scrollbar": {
-                  width: "5px",
-                },
-                "&::-webkit-scrollbar-track": {
-                  background: "transparent",
-                  borderRadius: "10px",
-                },
-                "&::-webkit-scrollbar-thumb": {
-                  background: "#888",
-                  borderRadius: "10px",
-                },
-                "&::-webkit-scrollbar-thumb:hover": {
-                  background: "#555",
-                },
-              }}
-            >
-              <FileUploadCard 
-                fileName="PRIDE. - Kendrick Lamar" 
-                fileType="mp3"
-                fileSizeInBytes={10830000} 
-                uploadFailed={false} 
-                inProgress={true}
-                progressValue={72}
-              />
-              <FileUploadCard 
-                fileName="Show Me How - Men I Trust"
-                fileType="mp3"
-                fileSizeInBytes={1120000}
-                uploadFailed={true}
-                inProgress={false}
-                progressValue={0}
-              />
-              <FileUploadCard 
-                fileName="No More Parties In LA - Kanye West" 
-                fileType="mp3"
-                fileSizeInBytes={14800000} 
-                uploadFailed={false} 
-                inProgress={false}
-                progressValue={100}
-              />
-              <FileUploadCard 
-                fileName="No More Parties In LA - Kanye West"
-                fileType="mp3" 
-                fileSizeInBytes={14800000} 
-                uploadFailed={false} 
-                inProgress={false}
-                progressValue={100}
-              />
-            </Box>
-
             {/* Modal footer */}
             <Flex justifyContent="space-between">
               <Button
@@ -176,7 +160,7 @@ export const FileUploadBox: React.FC<UploadBoxProps> = ({
                 size="lg"
                 variant="solid"
                 rounded={"xl"}
-                onClick={onClose}
+                onClick={onCardClose}
                 mr={4}
               >
                 Cancel
@@ -189,9 +173,9 @@ export const FileUploadBox: React.FC<UploadBoxProps> = ({
                 variant="solid"
                 rounded={"xl"}
                 color="brand.200"
-                onClick={onClose}
+                onClick={handleGenerate}
               >
-                Confirm
+                Generate
               </Button>
             </Flex>
           </ModalBody>
@@ -201,4 +185,4 @@ export const FileUploadBox: React.FC<UploadBoxProps> = ({
   );
 };
 
-export default FileUploadBox;
+export default FileCardGenerator;
