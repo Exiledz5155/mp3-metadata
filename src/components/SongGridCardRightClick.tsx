901af09b8eb6d73be2094ff48ab399e544c18572
Danny Bui <Dannybui5155@gmail.com>
2024-03-18 21:03:31 -0500

    cardref null handling

--- src/components/SongGridCardRightClick.tsx
+++ src/components/SongGridCardRightClick.tsx
@@ -1,28 +1,31 @@
 import {
-    Card,
-    Stack,
-    StackDivider,
-    Button,
-    Text,
-    Link
-  } from "@chakra-ui/react";
-  
+  Card,
+  Stack,
+  StackDivider,
+  Button,
+  Text,
+  Link,
+} from "@chakra-ui/react";
+
 import { ChangeEvent, useState, useEffect, useRef } from "react";
 
 /**
  * @param position x and y coordinates of the bottom left corner of the menu
- * @param onClose gets called when the menu should dissappear  
+ * @param onClose gets called when the menu should dissappear
  **/
-export function SongGridCardRightClick({ position, onClose}) {
+export function SongGridCardRightClick({ position, onClose }) {
   // Creates a reference to the Card component
   const cardRef = useRef(null);
   // Function that dynamically calculates and sets the top position of the Card component
   // based on the cursor position and the height of the Card itself.
   useEffect(() => {
-    const cardHeight = cardRef.current.clientHeight;
-    // Calculate menu height and set the top position
-    const topPosition = position.y - cardHeight;
-    cardRef.current.style.top = `${topPosition}px`;
+    if (cardRef.current) {
+      // Check if cardRef.current is not null
+      const cardHeight = cardRef.current.clientHeight;
+      // Calculate menu height and set the top position
+      const topPosition = position.y - cardHeight;
+      cardRef.current.style.top = `${topPosition}px`;
+    }
   }, [position, onClose]);
 
   //Event handler for when the mouse leaves the right click menue
@@ -34,7 +37,7 @@ export function SongGridCardRightClick({ position, onClose}) {
   const borderRad = "15px";
   const sizeOfFont = "md";
   return (
-    <Card 
+    <Card
       ref={cardRef} // Attaches a reference to the Card component
       variant={"elevated"}
       margin={"auto"}
@@ -46,199 +49,180 @@ export function SongGridCardRightClick({ position, onClose}) {
       borderRadius={borderRad}
       borderBottomLeftRadius="0"
     >
-      <Stack 
-        divider={<StackDivider />} 
+      <Stack
+        divider={<StackDivider />}
         spacing="0"
         bg={"brand.200"}
         borderRadius={borderRad}
         borderBottomLeftRadius="0"
       >
-        <Button 
-          style={{ 
+        <Button
+          style={{
             display: "flex",
             alignItems: "center",
             justifyContent: "flex-start",
-            borderRadius: borderRad, 
-            borderBottomLeftRadius: "0", 
-            borderBottomRightRadius: "0" 
+            borderRadius: borderRad,
+            borderBottomLeftRadius: "0",
+            borderBottomRightRadius: "0",
           }}
         >
-          <svg 
-            width="17px" 
-            height="17px" 
-            stroke-width="2" 
-            viewBox="0 0 24 24" 
-            fill="none" 
-            xmlns="http://www.w3.org/2000/svg" 
+          <svg
+            width="17px"
+            height="17px"
+            stroke-width="2"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
             color="#ffffff"
             style={{ marginRight: "0.5rem", marginLeft: "-0.5em" }}
           >
-          <path 
-            d="M12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2Z" 
-            stroke="#ffffff" 
-            stroke-width="2" 
-            stroke-linecap="round" 
-            stroke-linejoin="round"
-          >
-          </path>
-          <path 
-            d="M8 21.1679V14L12 7L16 14V21.1679" 
-            stroke="#ffffff" 
-            stroke-width="2" 
-            stroke-linecap="round" 
-            stroke-linejoin="round"
-          >
-          </path>
-          <path 
-            d="M8 14C8 14 9.12676 15 10 15C10.8732 15 12 14 12 14C12 14 13.1268 15 14 15C14.8732 15 16 14 16 14" 
-            stroke="#ffffff" 
-            stroke-width="2" 
-            stroke-linecap="round" 
-            stroke-linejoin="round"
-          >
-          </path>
+            <path
+              d="M12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2Z"
+              stroke="#ffffff"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            ></path>
+            <path
+              d="M8 21.1679V14L12 7L16 14V21.1679"
+              stroke="#ffffff"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            ></path>
+            <path
+              d="M8 14C8 14 9.12676 15 10 15C10.8732 15 12 14 12 14C12 14 13.1268 15 14 15C14.8732 15 16 14 16 14"
+              stroke="#ffffff"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            ></path>
           </svg>
-          <Text fontSize={sizeOfFont}>  
-            Edit
-          </Text>
+          <Text fontSize={sizeOfFont}>Edit</Text>
         </Button>
-        <Button 
-          style={{ 
+        <Button
+          style={{
             display: "flex",
             alignItems: "center",
             justifyContent: "flex-start",
-            borderRadius: 0 
+            borderRadius: 0,
           }}
         >
-          <svg 
-            width="17px" 
-            height="17px" 
-            stroke-width="2.0" 
-            viewBox="0 0 24 24" 
-            fill="none" 
-            xmlns="http://www.w3.org/2000/svg" 
+          <svg
+            width="17px"
+            height="17px"
+            stroke-width="2.0"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
             color="#ffffff"
             style={{ marginRight: "0.5rem", marginLeft: "-0.5em" }}
           >
-            <path 
-              d="M12 11.5V16.5" 
-              stroke="#ffffff" 
-              stroke-width="2.0" 
-              stroke-linecap="round" 
+            <path
+              d="M12 11.5V16.5"
+              stroke="#ffffff"
+              stroke-width="2.0"
+              stroke-linecap="round"
               stroke-linejoin="round"
-            >
-            </path>
-            <path 
-              d="M12 7.51L12.01 7.49889" 
-              stroke="#ffffff" 
-              stroke-width="2.0" 
-              stroke-linecap="round" 
+            ></path>
+            <path
+              d="M12 7.51L12.01 7.49889"
+              stroke="#ffffff"
+              stroke-width="2.0"
+              stroke-linecap="round"
               stroke-linejoin="round"
-            >
-            </path>
-            <path 
-              d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" 
-              stroke="#ffffff" 
-              stroke-width="2.0" 
-              stroke-linecap="round" 
+            ></path>
+            <path
+              d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
+              stroke="#ffffff"
+              stroke-width="2.0"
+              stroke-linecap="round"
               stroke-linejoin="round"
-            >
-            </path>
+            ></path>
           </svg>
-          <Text fontSize={sizeOfFont}>  
-            Properties
-          </Text>
+          <Text fontSize={sizeOfFont}>Properties</Text>
         </Button>
-        <Button 
-          style={{ 
+        <Button
+          style={{
             display: "flex",
             alignItems: "center",
             justifyContent: "flex-start",
-            borderRadius: 0
+            borderRadius: 0,
           }}
         >
-          <svg 
-            width="17px" 
-            height="17px" 
-            stroke-width="2" 
-            viewBox="0 0 24 24" 
-            fill="none" 
-            xmlns="http://www.w3.org/2000/svg" 
+          <svg
+            width="17px"
+            height="17px"
+            stroke-width="2"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
             color="#ffffff"
             style={{ marginRight: "0.5rem", marginLeft: "-0.5em" }}
           >
-            <path 
-              d="M9 17L15 17" 
-              stroke="#ffffff" 
-              stroke-width="2" 
-              stroke-linecap="round" 
+            <path
+              d="M9 17L15 17"
+              stroke="#ffffff"
+              stroke-width="2"
+              stroke-linecap="round"
               stroke-linejoin="round"
-            >
-            </path>
-            <path 
-              d="M12 6V13M12 13L15.5 9.5M12 13L8.5 9.5" 
-              stroke="#ffffff" 
-              stroke-width="2" 
-              stroke-linecap="round" 
+            ></path>
+            <path
+              d="M12 6V13M12 13L15.5 9.5M12 13L8.5 9.5"
+              stroke="#ffffff"
+              stroke-width="2"
+              stroke-linecap="round"
               stroke-linejoin="round"
-            >
-            </path>
-            <path 
-              d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" 
-              stroke="#ffffff" 
-              stroke-width="2" 
-              stroke-linecap="round" 
+            ></path>
+            <path
+              d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
+              stroke="#ffffff"
+              stroke-width="2"
+              stroke-linecap="round"
               stroke-linejoin="round"
-            >
-            </path>
+            ></path>
           </svg>
-          <Text fontSize={sizeOfFont}>  
-            Download
-          </Text>
+          <Text fontSize={sizeOfFont}>Download</Text>
         </Button>
-        <Button 
-          style={{ 
+        <Button
+          style={{
             display: "flex",
             alignItems: "center",
             justifyContent: "flex-start",
-            borderRadius: borderRad, 
-            borderTopLeftRadius: "0", 
-            borderTopRightRadius: "0", 
-            borderBottomLeftRadius: "0" 
+            borderRadius: borderRad,
+            borderTopLeftRadius: "0",
+            borderTopRightRadius: "0",
+            borderBottomLeftRadius: "0",
           }}
         >
-        <svg 
-          width="17px"
-          height="17px" 
-          stroke-width="2" 
-          viewBox="0 0 24 24" 
-          fill="none" 
-          xmlns="http://www.w3.org/2000/svg" 
-          color="#ffffff"
-          style={{ marginRight: "0.5rem", marginLeft: "-0.5em" }}
-        >
-          <path 
-            d="M8 12H16" 
-            stroke="#ffffff" 
-            stroke-width="2" 
-            stroke-linecap="round" 
-            stroke-linejoin="round"
+          <svg
+            width="17px"
+            height="17px"
+            stroke-width="2"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            color="#ffffff"
+            style={{ marginRight: "0.5rem", marginLeft: "-0.5em" }}
           >
-          </path>
-          <path 
-            d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" 
-            stroke="#ffffff" 
-            stroke-width="2" 
-            stroke-linecap="round" 
-            stroke-linejoin="round"
-          >
-          </path>
-        </svg>
-          <Text fontSize={sizeOfFont}>  
-            Remove
-          </Text>
+            <path
+              d="M8 12H16"
+              stroke="#ffffff"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            ></path>
+            <path
+              d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
+              stroke="#ffffff"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            ></path>
+          </svg>
+          <Text fontSize={sizeOfFont}>Remove</Text>
         </Button>
-      </Stack> 
+      </Stack>
     </Card>
   );
 }
