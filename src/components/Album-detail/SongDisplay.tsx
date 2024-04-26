2d55f8f57b375d58a5654a1a165cfcddc7b1f860
Danny Bui <Dannybui5155@gmail.com>
2024-04-26 02:46:03 -0500

    sorting with buttons and icons working

--- src/components/Album-detail/SongDisplay.tsx
+++ src/components/Album-detail/SongDisplay.tsx
@@ -10,6 +10,11 @@ import {
   background,
   Spinner,
   useToast,
+  Flex,
+  HStack,
+  Tooltip,
+  Text,
+  Button,
 } from "@chakra-ui/react";
 import { SongGridCard } from "./SongGridCard";
 import { SongGridLabel } from "./SongGridLabel";
@@ -20,7 +25,14 @@ import ActionMenu from "../Actions/ActionMenu";
 import Edit from "../Actions/Edit";
 import Properties from "../Actions/Properties";
 import { useUUID } from "../../contexts/UUIDContext";
-import { CheckCircleIcon, WarningIcon } from "@chakra-ui/icons";
+import {
+  CheckCircleIcon,
+  ChevronDownIcon,
+  ChevronUpIcon,
+  Icon,
+  TimeIcon,
+  WarningIcon,
+} from "@chakra-ui/icons";
 
 export function SongDisplay({ album }: { album: Album }) {
   const { uuid } = useUUID();
@@ -29,9 +41,67 @@ export function SongDisplay({ album }: { album: Album }) {
   const [rightClickPosition, setRightClickPosition] = useState({ x: 0, y: 0 });
   const [isEditModalOpen, setIsEditModalOpen] = useState(false);
   const [isPropertiesModalOpen, setIsPropertiesModalOpen] = useState(false);
+  const [sortCriteria, setSortCriteria] = useState({
+    field: "trackNumber",
+    order: "asc",
+  });
+
+  // sorting songs
+  const getSortedSongs = () => {
+    return [...album.songs].sort((a, b) => {
+      let aValue = a[sortCriteria.field];
+      let bValue = b[sortCriteria.field];
+
+      // If sorting by duration, convert the string to an integer
+      if (sortCriteria.field === "duration") {
+        aValue = parseInt(aValue);
+        bValue = parseInt(bValue);
+      }
+
+      const isAsc = sortCriteria.order === "asc";
+      return isAsc ? aValue - bValue : bValue - aValue;
+    });
+  };
+
+  const sortedSongs = getSortedSongs();
+
+  const handleSortChange = (field) => {
+    setSortCriteria((prev) => ({
+      field,
+      order: prev.field === field && prev.order === "asc" ? "desc" : "asc",
+    }));
+  };
+
+  const SortButton = ({ field, children, w = "auto", center = false }) => {
+    const isActive = sortCriteria.field === field;
+    const directionIcon =
+      sortCriteria.order === "asc" ? (
+        <ChevronUpIcon color={"green.200"} />
+      ) : (
+        <ChevronDownIcon color={"red.300"} />
+      );
+
+    return (
+      <Button
+        variant="unstyled"
+        onClick={() => handleSortChange(field)}
+        _focus={{ boxShadow: "none" }}
+        w={w}
+      >
+        <Flex
+          align="center"
+          justify={center ? "center" : "start"}
+          color="white"
+          w="100%"
+        >
+          {children}
+          {isActive && directionIcon}
+        </Flex>
+      </Button>
+    );
+  };
 
   // Action Menu handling
-
   const openEditModal = () => {
     setIsEditModalOpen(true);
   };
@@ -48,18 +118,30 @@ export function SongDisplay({ album }: { album: Album }) {
     setIsPropertiesModalOpen(false);
   };
 
+  // Selection handling
   const handleSelectSong = (
     songId: string,
     event: React.MouseEvent<HTMLDivElement>
   ) => {
+    // Get the current sorted order of songs
+    const currentSortedOrder = getSortedSongs();
+
     if (event.shiftKey) {
-      const allSongIds = album.songs.map((song) => song.id);
+      // Find indices in the sorted array
       const lastSelectedId = selectedSongs[selectedSongs.length - 1];
-      const currentIdx = allSongIds.indexOf(songId);
-      const lastIdx = allSongIds.indexOf(lastSelectedId);
-      const start = Math.min(currentIdx, lastIdx);
-      const end = Math.max(currentIdx, lastIdx);
-      setSelectedSongs(allSongIds.slice(start, end + 1));
+      const currentIdx = currentSortedOrder.findIndex(
+        (song) => song.id === songId
+      );
+      const lastIdx = currentSortedOrder.findIndex(
+        (song) => song.id === lastSelectedId
+      );
+      const range = [currentIdx, lastIdx].sort((a, b) => a - b);
+
+      // Select the range of songs based on sorted order
+      const newSelectedSongs = currentSortedOrder
+        .slice(range[0], range[1] + 1)
+        .map((song) => song.id);
+      setSelectedSongs(newSelectedSongs);
     } else if (event.ctrlKey || event.metaKey) {
       setSelectedSongs((prev) =>
         prev.includes(songId)
@@ -87,8 +169,8 @@ export function SongDisplay({ album }: { album: Album }) {
     setRightClickPosition({ x: event.clientX, y: event.clientY });
   };
 
+  // Handle download
   const toast = useToast();
-
   const handleDownload = async () => {
     const songIds = selectedSongObjects.map((song) => song.id);
     const downloadUrl = `/api/download?uuid=${uuid}&ids=${songIds.join(",")}`;
@@ -212,9 +294,57 @@ export function SongDisplay({ album }: { album: Album }) {
       maxHeight="calc(100vh - 90px)"
       overflow={"hidden"}
     >
-      {/* UPDATE THIS WITH PROPS */}
-      <AlbumInfoSection album={album}></AlbumInfoSection>
-      <SongGridLabel></SongGridLabel>
+      <AlbumInfoSection album={album} />
+      <HStack
+        align={"left"}
+        borderRadius={"10px"}
+        position={"sticky"}
+        bg="brand.100"
+        top="0px"
+        mt={5}
+      >
+        <Flex align={"center"} w="30%">
+          <Text
+            as={"button"}
+            fontSize={"md"}
+            mx={"4"}
+            color={"white"}
+            onClick={() => handleSortChange("trackNumber")}
+            sx={{ userSelect: "none" }} // Disables text selection
+          >
+            #
+          </Text>
+          <SortButton field="title">
+            <Text textAlign={"left"} noOfLines={1}>
+              Title
+            </Text>
+          </SortButton>
+        </Flex>
+        <SortButton field="artist" w="30%">
+          <Text textAlign={"left"} noOfLines={1}>
+            Artist(s)
+          </Text>
+        </SortButton>
+        <SortButton field="album" w="30%">
+          <Text textAlign={"left"} noOfLines={1}>
+            Album
+          </Text>
+        </SortButton>
+        <SortButton field="duration" w="10%" center={true}>
+          <Text textAlign={"center"} noOfLines={1}>
+            <Tooltip
+              hasArrow
+              label="Duration"
+              aria-label="Duration"
+              placement="top"
+              bg={"brand.300"}
+              color={"white"}
+            >
+              <Icon as={TimeIcon} />
+            </Tooltip>
+          </Text>
+        </SortButton>
+      </HStack>
       <Divider position={"sticky"} top="6" mb={2} />
       <CardBody
         m={"0"}
@@ -240,14 +370,13 @@ export function SongDisplay({ album }: { album: Album }) {
         }}
       >
         <Box>
-          {album.songs.map((song, index) => (
+          {sortedSongs.map((song, index) => (
             <SongGridCard
               key={song.id}
               song={song}
               isSelected={selectedSongs.includes(song.id)}
               onClick={handleSelectSong}
               onRightClick={handleRightClick}
-              // isLast={index === album.songs.length - 1}
             />
           ))}
         </Box>
@@ -277,3 +406,322 @@ export function SongDisplay({ album }: { album: Album }) {
     </Card>
   );
 }
+
+// // app/providers.tsx
+// "use client";
+
+// import {
+//   Card,
+//   CardBody,
+//   Divider,
+//   Box,
+//   Toast,
+//   background,
+//   Spinner,
+//   useToast,
+//   Flex,
+//   HStack,
+//   Tooltip,
+//   Text,
+// } from "@chakra-ui/react";
+// import { SongGridCard } from "./SongGridCard";
+// import { SongGridLabel } from "./SongGridLabel";
+// import { AlbumInfoSection } from "./AlbumInfoSection";
+// import { Album, Song } from "../../types/types";
+// import { useState } from "react";
+// import ActionMenu from "../Actions/ActionMenu";
+// import Edit from "../Actions/Edit";
+// import Properties from "../Actions/Properties";
+// import { useUUID } from "../../contexts/UUIDContext";
+// import { CheckCircleIcon, Icon, TimeIcon, WarningIcon } from "@chakra-ui/icons";
+
+// export function SongDisplay({ album }: { album: Album }) {
+//   const { uuid } = useUUID();
+//   const [selectedSongs, setSelectedSongs] = useState<string[]>([]);
+//   const [rightClickedSong, setRightClickedSong] = useState<Song | null>(null);
+//   const [rightClickPosition, setRightClickPosition] = useState({ x: 0, y: 0 });
+//   const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+//   const [isPropertiesModalOpen, setIsPropertiesModalOpen] = useState(false);
+
+//   // Action Menu handling
+
+//   const openEditModal = () => {
+//     setIsEditModalOpen(true);
+//   };
+
+//   const closeEditModal = () => {
+//     setIsEditModalOpen(false);
+//   };
+
+//   const openPropertiesModal = () => {
+//     setIsPropertiesModalOpen(true);
+//   };
+
+//   const closePropertiesModal = () => {
+//     setIsPropertiesModalOpen(false);
+//   };
+
+//   const handleSelectSong = (
+//     songId: string,
+//     event: React.MouseEvent<HTMLDivElement>
+//   ) => {
+//     if (event.shiftKey) {
+//       const allSongIds = album.songs.map((song) => song.id);
+//       const lastSelectedId = selectedSongs[selectedSongs.length - 1];
+//       const currentIdx = allSongIds.indexOf(songId);
+//       const lastIdx = allSongIds.indexOf(lastSelectedId);
+//       const start = Math.min(currentIdx, lastIdx);
+//       const end = Math.max(currentIdx, lastIdx);
+//       setSelectedSongs(allSongIds.slice(start, end + 1));
+//     } else if (event.ctrlKey || event.metaKey) {
+//       setSelectedSongs((prev) =>
+//         prev.includes(songId)
+//           ? prev.filter((id) => id !== songId)
+//           : [...prev, songId]
+//       );
+//     } else {
+//       setSelectedSongs([songId]);
+//     }
+//   };
+
+//   const handleRightClick = (
+//     songId: string,
+//     event: React.MouseEvent<HTMLDivElement>
+//   ) => {
+//     event.preventDefault();
+//     const song = album.songs.find((song) => song.id === songId);
+//     if (!song) return;
+
+//     if (!selectedSongs.includes(songId)) {
+//       setSelectedSongs([songId]);
+//     }
+
+//     setRightClickedSong(song);
+//     setRightClickPosition({ x: event.clientX, y: event.clientY });
+//   };
+
+//   const toast = useToast();
+
+//   const handleDownload = async () => {
+//     const songIds = selectedSongObjects.map((song) => song.id);
+//     const downloadUrl = `/api/download?uuid=${uuid}&ids=${songIds.join(",")}`;
+
+//     try {
+//       const downloadPromise = fetch(downloadUrl).then(async (response) => {
+//         if (response.ok) {
+//           const blob = await response.blob();
+//           const url = window.URL.createObjectURL(blob);
+//           const link = document.createElement("a");
+//           link.href = url;
+//           link.setAttribute("download", "songs.zip");
+//           document.body.appendChild(link);
+//           link.click();
+//           link.remove();
+//         } else {
+//           throw new Error("Download failed.");
+//         }
+//       });
+
+//       // toast.promise(downloadPromise, {
+//       //   loading: {
+//       //     title: "Download in Progress",
+//       //     description: "Please wait while your songs are being downloaded.",
+//       //     containerStyle: {
+
+//       //     },
+//       //   },
+//       //   success: {
+//       //     title: "Download Completed",
+//       //     description: "Your songs have been downloaded successfully.",
+//       //   },
+//       //   error: {
+//       //     title: "Download Failed",
+//       //     description: "An error occurred while downloading your songs.",
+//       //   },
+//       // });
+//       toast.promise(downloadPromise, {
+//         loading: {
+//           render: () => (
+//             <Box
+//               display="flex"
+//               alignItems="center"
+//               // bg="linear.200"
+//               bgGradient="linear(to-r, linear.100, linear.200)"
+//               color="black"
+//               p={3}
+//               borderRadius="md"
+//               boxShadow="lg"
+//             >
+//               <Spinner size="md" mr={3} />
+//               <Box>
+//                 <strong>Download in Progress</strong>
+//                 <br />
+//                 Please wait while your songs are being downloaded.
+//               </Box>
+//             </Box>
+//           ),
+//         },
+//         success: {
+//           render: () => (
+//             <Box
+//               display="flex"
+//               alignItems="center"
+//               bg="green.200"
+//               color="black"
+//               p={3}
+//               borderRadius="md"
+//               boxShadow="lg"
+//             >
+//               <CheckCircleIcon boxSize={6} mr={3} />
+//               <Box>
+//                 <strong>Download Completed</strong>
+//                 <br />
+//                 Your songs have been downloaded successfully.
+//               </Box>
+//             </Box>
+//           ),
+//         },
+//         error: {
+//           render: () => (
+//             <Box
+//               display="flex"
+//               alignItems="center"
+//               bg="red.300"
+//               color="black"
+//               p={3}
+//               borderRadius="md"
+//               boxShadow="lg"
+//             >
+//               <WarningIcon boxSize={6} mr={3} />
+//               <Box>
+//                 <strong>Download Failed</strong>
+//                 <br />
+//                 An error occurred while downloading your songs.
+//               </Box>
+//             </Box>
+//           ),
+//         },
+//       });
+//     } catch (error) {
+//       console.error("Error during download:", error);
+//     }
+//   };
+
+//   const closeMenu = () => {
+//     setRightClickedSong(null);
+//   };
+
+//   // Map selected song IDs to Song objects
+//   const selectedSongObjects = selectedSongs
+//     .map((id) => album.songs.find((song) => song.id === id))
+//     .filter((song): song is Song => song !== undefined);
+
+//   return (
+//     <Card
+//       p={"20px"}
+//       bg="brand.100"
+//       h="100%"
+//       rounded={"xl"}
+//       maxHeight="calc(100vh - 90px)"
+//       overflow={"hidden"}
+//     >
+//       <AlbumInfoSection album={album}></AlbumInfoSection>
+//       {/* <SongGridLabel></SongGridLabel> */}
+//       {/* SONG GRID LABEL */}
+//       <HStack
+//         align={"left"}
+//         borderRadius={"10px"}
+//         position={"sticky"}
+//         bg="brand.100"
+//         top="0px"
+//         mt={5}
+//       >
+//         <Flex align={"left"} w="30%">
+//           <Text fontSize={"md"} mx={"4"} color={"white"}>
+//             #
+//           </Text>
+//           <Text textAlign={"left"} noOfLines={1} color={"white"}>
+//             Title
+//           </Text>
+//         </Flex>
+//         <Text textAlign={"left"} noOfLines={1} w="30%" color={"white"}>
+//           Artist(s)
+//         </Text>
+//         <Text textAlign={"left"} noOfLines={1} w="30%" color={"white"}>
+//           Album
+//         </Text>
+//         <Text textAlign={"center"} noOfLines={1} w="10%">
+//           <Tooltip
+//             hasArrow
+//             label="Duration"
+//             aria-label="Duration"
+//             placement="top"
+//             bg={"brand.300"}
+//             color={"white"}
+//           >
+//             <Icon as={TimeIcon} color={"white"} />
+//           </Tooltip>
+//         </Text>
+//       </HStack>
+//       <Divider position={"sticky"} top="6" mb={2} />
+//       <CardBody
+//         m={"0"}
+//         p={"0"}
+//         display={"flex"}
+//         flexDirection={"column"}
+//         overflow={"auto"}
+//         css={{
+//           "&::-webkit-scrollbar": {
+//             width: "5px",
+//           },
+//           "&::-webkit-scrollbar-track": {
+//             background: "transparent",
+//             borderRadius: "10px",
+//           },
+//           "&::-webkit-scrollbar-thumb": {
+//             background: "#888",
+//             borderRadius: "10px",
+//           },
+//           "&::-webkit-scrollbar-thumb:hover": {
+//             background: "#555",
+//           },
+//         }}
+//       >
+//         <Box>
+//           {album.songs.map((song, index) => (
+//             <SongGridCard
+//               key={song.id}
+//               song={song}
+//               isSelected={selectedSongs.includes(song.id)}
+//               onClick={handleSelectSong}
+//               onRightClick={handleRightClick}
+//               // isLast={index === album.songs.length - 1}
+//             />
+//           ))}
+//         </Box>
+//       </CardBody>
+//       {rightClickedSong && (
+//         <ActionMenu
+//           songs={selectedSongObjects}
+//           position={rightClickPosition}
+//           onClose={closeMenu}
+//           onEditClick={openEditModal}
+//           onPropertiesClick={openPropertiesModal}
+//           onDownloadClick={handleDownload}
+//         />
+//       )}
+//       {/* I have no idea why this works do not delete it.
+//       All hail Claude */}
+//       <Edit
+//         isOpen={isEditModalOpen}
+//         onClose={closeEditModal}
+//         songs={selectedSongObjects}
+//       />
+//       <Properties
+//         isOpen={isPropertiesModalOpen}
+//         onClose={closePropertiesModal}
+//         songs={selectedSongObjects}
+//       />
+//     </Card>
+//   );
+// }
