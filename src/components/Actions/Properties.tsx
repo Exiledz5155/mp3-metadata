be23da7cca38ffdd65cef95c38b21918f70c1a1b
Danny Bui <Dannybui5155@gmail.com>
2024-06-16 16:09:26 -0500

    Only show tooltip when text truncated

--- src/components/Actions/Properties.tsx
+++ src/components/Actions/Properties.tsx
@@ -14,7 +14,7 @@ import {
   Tooltip,
   VStack,
 } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { CommonSongProperties, Song } from "../../types/types";
 import { calculateCommonProperties } from "../../util/commonprops";
 import { calculateTotalDuration } from "../../util/duration";
@@ -37,30 +37,45 @@ export default function Properties({
     setCommonProperties(calculateCommonProperties(songs));
   }, [songs]);
 
-  const PropertyRow = ({ label, value, hasTooltip }) => (
-    <HStack justifyContent="space-between">
-      <Text fontWeight="semibold">{label}</Text>
-      {hasTooltip ? (
-        <Tooltip
-          label={value}
-          placement="right"
-          bg={"brand.300"}
-          color={"white"}
-          borderRadius={5}
-          p={2}
-          hasArrow
-        >
-          <Text noOfLines={1} maxW={"70%"}>
+  const PropertyRow = ({ label, value }) => {
+    const textRef = useRef<HTMLDivElement>(null);
+    const [isTruncated, setIsTruncated] = useState(false);
+
+    useEffect(() => {
+      const element = textRef.current;
+      if (element) {
+        const isOverflowing =
+          element.scrollWidth > element.clientWidth ||
+          element.scrollHeight > element.clientHeight;
+        setIsTruncated(isOverflowing);
+      }
+    }, [value]);
+
+    return (
+      <HStack justifyContent="space-between">
+        <Text fontWeight="semibold">{label}</Text>
+        {isTruncated ? (
+          <Tooltip
+            label={value}
+            placement="right"
+            bg={"brand.300"}
+            color={"white"}
+            borderRadius={5}
+            p={2}
+            hasArrow
+          >
+            <Text ref={textRef} noOfLines={1} maxW={"70%"}>
+              {value}
+            </Text>
+          </Tooltip>
+        ) : (
+          <Text ref={textRef} noOfLines={1} maxW={"70%"}>
             {value}
           </Text>
-        </Tooltip>
-      ) : (
-        <Text noOfLines={1} maxW={"70%"}>
-          {value}
-        </Text>
-      )}
-    </HStack>
-  );
+        )}
+      </HStack>
+    );
+  };
 
   return (
     <>
@@ -71,56 +86,38 @@ export default function Properties({
         size="xl"
       >
         <ModalOverlay />
-        <ModalContent bg={"brand.200"} pb={25} borderRadius={"xl"}>
-          <ModalHeader>Properties</ModalHeader>
-          <ModalCloseButton />
+        <ModalContent bg={"brand.200"} py={25} borderRadius={"xl"}>
+          <ModalHeader pt={0}>Properties</ModalHeader>
+          <ModalCloseButton mr={3} mt={3} />
           <ModalBody>
             <VStack spacing={4} align="stretch">
-              <PropertyRow
-                label="Title"
-                value={commonProperties.title || ""}
-                hasTooltip={true}
-              />
+              <PropertyRow label="Title" value={commonProperties.title || ""} />
               <PropertyRow
                 label="Artist(s)"
                 value={commonProperties.artist || ""}
-                hasTooltip={true}
               />
               <PropertyRow
                 label="Album"
                 value={commonProperties.albumTitle || ""}
-                hasTooltip={true}
               />
               <PropertyRow
                 label="Album Artist(s)"
                 value={commonProperties.albumArtist || ""}
-                hasTooltip={true}
               />
               <PropertyRow
                 label="Year"
                 value={commonProperties.year?.toString() || ""}
-                hasTooltip={false}
               />
-              <PropertyRow
-                label="Genre"
-                value={commonProperties.genre || ""}
-                hasTooltip={false}
-              />
+              <PropertyRow label="Genre" value={commonProperties.genre || ""} />
               <PropertyRow
                 label="Track"
                 value={commonProperties.trackNumber?.toString() || ""}
-                hasTooltip={false}
               />
               <PropertyRow
                 label="Length"
                 value={calculateTotalDuration(songs) || ""}
-                hasTooltip={false}
               />
-              <PropertyRow
-                label="dev_id"
-                value={commonProperties.id || ""}
-                hasTooltip={false}
-              />
+              <PropertyRow label="dev_id" value={commonProperties.id || ""} />
             </VStack>
           </ModalBody>
 
