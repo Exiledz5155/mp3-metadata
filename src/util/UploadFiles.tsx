4de5aa434e5128eff37e2ede12d3b12e83aa8456
Danny Bui <Dannybui5155@gmail.com>
2024-06-17 21:28:32 -0500

    remove commented code

--- src/util/UploadFiles.tsx
+++ src/util/UploadFiles.tsx
@@ -1,29 +1,3 @@
-// // Assumes file type handling prior to function call
-// export async function UploadMP3(
-//   file: File,
-//   userUUID: string,
-//   onProgress: (progress: number) => void
-// ): Promise<Response> {
-//   // create file path using userUUID and file name
-//   const userFilePath = `${userUUID}/${encodeURIComponent(file.name)}`;
-
-//   const formData = new FormData();
-//   formData.append("file", file);
-//   formData.append("userFilePath", userFilePath);
-
-//   const response = await fetch(`/api/upload/mp3?fileName=${userFilePath}`, {
-//     method: "POST",
-//     body: formData,
-//   });
-
-//   if (!response.ok) {
-//     throw new Error("Failed to upload file");
-//   }
-
-//   return response;
-// }
-
-// Assumes file type handling prior to function call
 // Assumes file type handling prior to function call
 export async function UploadMP3(
   file: File,
